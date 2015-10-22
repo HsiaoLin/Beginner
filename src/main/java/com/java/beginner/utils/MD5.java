6dd21a9aa5dadac156662943758327881ff64360
yinxiaoling <yinxiaoling@shijie99.com>
2015-10-22 20:17:40 +0800

    更新

---
+++ src/main/java/com/java/beginner/utils/MD5.java
@@ -0,0 +1,99 @@
+package com.java.beginner.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class MD5 {
+
+	public static String md5(String str) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes());
+			byte b[] = md.digest();
+
+			int i;
+
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < b.length; offset++) {
+				i = b[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+			str = buf.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return str;
+	}
+
+	/**
+	 * sign(方法描述：签名字符串) <br />
+	 * (方法适用条件描述： – 可选)
+	 * @param text 需要签名的字符串
+	 * @param key 密钥
+	 * @param input_charset 编码格式
+	 * @return
+	 * String 签名结果
+	 * @exception
+	 * @since  1.0.0
+	 */
+	public static String sign(String text, String key, String input_charset) {
+		text = text + key;
+		return DigestUtils.md5Hex(getContentBytes(text, input_charset));
+	}
+
+	/**
+	 * verify(方法描述：签名字符串) <br />
+	 * (方法适用条件描述： – 可选)
+	 * @param text 需要签名的字符串
+	 * @param sign 签名结果
+	 * @param key 密钥
+	 * @param input_charset 编码格式
+	 * @return
+	 * boolean 签名结果
+	 * @exception
+	 * @since  1.0.0
+	 */
+	public static boolean verify(String text, String sign, String key, String input_charset) {
+		text = text + key;
+		String mysign = DigestUtils.md5Hex(getContentBytes(text, input_charset));
+		if (mysign.equals(sign)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	* getContentBytes(方法描述：编码) <br />
+	* (方法适用条件描述： – 可选)
+	* @param content
+	* @param charset
+	* @return
+	* byte[]
+	* @exception
+	* @since  1.0.0
+	*/
+	private static byte[] getContentBytes(String content, String charset) {
+		if (charset == null || "".equals(charset)) {
+			return content.getBytes();
+		}
+		try {
+			return content.getBytes(charset);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "test" + "jp150107113" + "RTEHKA" + "4";
+		System.out.println(str);
+		System.out.println(md5(str));
+	}
+}
