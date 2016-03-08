e78b8bac1ddb9f0533996b0e809f74270e6e59a0
yinxiaoling <350231275@qq.com>
2016-03-08 23:24:58 +0800

    1

--- src/test/java/com/beginner/base/utils/Test.java
+++ src/test/java/com/beginner/base/utils/Test.java
@@ -1,5 +1,7 @@
 package com.beginner.base.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,19 +17,30 @@ public class Test {
 	public static void main(String[] args) throws InterruptedException {
 		//http://www.cnblogs.com/wanqieddy/p/3853863.html
 		ExecutorService executor = Executors.newFixedThreadPool(10);
-		CountDownLatch count = new CountDownLatch(10);
+
+		final List list = new ArrayList();
+
+		final CountDownLatch count = new CountDownLatch(10);
 		for (int i = 0; i < 10; i++) {
 			executor.execute(new Runnable() {
 
 				@Override
 				public void run() {
-					logger.info(new Test().toString());
+					try {
+						list.add(new Test().toString());
+						logger.info(new Test().toString());
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						count.countDown();
+					}
 				}
-
 			});
-			count.countDown();
 		}
-		count.await(10, TimeUnit.SECONDS);
+		count.await(3, TimeUnit.SECONDS);
+
+		logger.info("list.size()={}", list.size());
 		logger.info("是否终止:" + executor.isTerminated());
 		logger.info("是否关闭:" + executor.isShutdown());
 		if (executor.isShutdown()) {
@@ -37,6 +50,7 @@ public class Test {
 		logger.info("是否终止:" + executor.isTerminated());
 		logger.info("是否关闭:" + executor.isShutdown());
 	}
+
 	@Override
 	public String toString() {
 		return "我是Test";
