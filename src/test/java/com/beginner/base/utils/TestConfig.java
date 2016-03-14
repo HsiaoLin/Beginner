a6d2bc403d72a65114acbdb15bfe88f5d54901bc
yinxiaoling <yinxiaoling@shijie99.com>
2016-03-14 18:10:51 +0800

    1

---
+++ src/test/java/com/beginner/base/utils/TestConfig.java
@@ -0,0 +1,18 @@
+package com.beginner.base.utils;
+
+public class TestConfig {
+
+	public static void main(String[] args) {
+		/*
+		 * 配置中心-zookeeper方案初版：
+		 * 
+		 * TODO：1、解决多项目部署
+		 * TODO：2、解决分布式部署
+		 * TODO：3、解决本地化配置
+		 * TODO：4、提供存储、通知、查询基础功能
+		 * TODO：5、解决平滑升级-要有完善的版本管理机制
+		 * TODO：6、解决侵入式设计-jar包、监听等等方案
+		 * TODO：7、整体规划、流程规划、平滑升级规划
+		 */
+	}
+}
