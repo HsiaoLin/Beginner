dd7cf98616bd73a907d205a6cc050e799ae995e2
yinxiaoling <yinxiaoling@shijie99.com>
2016-03-15 18:07:56 +0800

    111

--- src/test/java/com/beginner/base/utils/TestConfig.java
+++ src/test/java/com/beginner/base/utils/TestConfig.java
@@ -13,6 +13,10 @@ public class TestConfig {
 		 * TODO：5、解决平滑升级-要有完善的版本管理机制
 		 * TODO：6、解决侵入式设计-jar包、监听等等方案
 		 * TODO：7、整体规划、流程规划、平滑升级规划
+		 * TODO：8、解决高可用
+		 * TODO：9、解决低延迟
+		 * TODO：10、解决时效性
+		 * dubbo如何使用zookeeper
 		 */
 	}
 }
